b754e9ce3d6bc8c30f08889d55f8877f84505724
stefanelli990 <stefan.mohenski@gmail.com>
2023-11-15 23:50:29 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <AppHeader/>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+
+</script>
+
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <header>
+        <h1 class="bg-red-600">Expense Tracker</h1>
+    </header>
+</template>
