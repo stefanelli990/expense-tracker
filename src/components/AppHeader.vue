256470a1c4827aac2e897f45eb13a32c389fd7d7
stefanelli990 <stefan.mohenski@gmail.com>
2024-05-02 11:55:14 +0200

    app renamed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
     <header class="max-w-5xl mx-auto px-4 py-8 flex justify-center">
-        <h1 class="text-3xl font-bold">ExpenseVue</h1>
+        <h1 class="text-3xl font-bold">Expense Tracker</h1>
     </header>
 </template>
